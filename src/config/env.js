417635d12c62756f4d611edd71c3916c8dc95d00
luke2327 <>
2023-03-30 13:17:31 +0900

    feat: configuration files

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: 6050,
-
   collectionFileStream: "/../collection/lmsList.json",
   fileEncoding: "utf-8",
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 const lms = require("./provider/lms");
 const {
   PORT
